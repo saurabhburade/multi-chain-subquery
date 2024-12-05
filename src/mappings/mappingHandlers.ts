aaa94e904a631091aa5f1f0d1de5330ce3b55086
Saurabh Burade <sburade3@gmail.com>
2024-12-05 23:29:23 +0530

    chore: refactor

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -76,9 +76,7 @@ export async function handleBlock(block: SubstrateBlock): Promise<void> {
       logger.info(
         `New DATA AVAIL ::::::  ${dataSubmissionSize.toString()} APP:: ${extrinsic
           .inspect()
-          .outer!.toString()} SIGNER :: ${extrinsic.signer.toString()} HASH:: ${extrinsic.hash.toString()} ARGS:: ${
-          methodData.args
-        }`
+          .outer!.toString()} SIGNER :: ${extrinsic.signer.toString()} HASH:: ${extrinsic.hash.toString()}`
       );
       logger.info(`New DATA AVAIL ::::::  ${dataSubmissionSize.toString()}`);
       // idx,
