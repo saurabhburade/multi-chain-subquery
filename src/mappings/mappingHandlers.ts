055f5b179ff1e662aa49c2cc2b373d188d45e9e9
Saurabh Burade <sburade3@gmail.com>
2024-12-07 23:32:52 +0530

    fix: api call

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -89,9 +89,9 @@ export async function handleBlock(block: CorrectSubstrateBlock): Promise<void> {
   const minuteId = Math.floor(blockDate.getTime() / 60000);
   if (blockRecord === undefined || blockRecord === null) {
     try {
-      const httpData = await fetch("https://api.github.com/users/github");
+      const httpData = await fetch("https://api.github.com/users/github", {});
       const jsonData = await httpData.json();
-      logger.info(`httpData: ${JSON.stringify(jsonData)}`);
+      logger.info(`httpData: ${(jsonData as any).name}`);
       const provider = new ethers.providers.JsonRpcProvider(
         "https://lb.drpc.org/ogrpc?network=ethereum&dkey=ArT8p5S52UM0rgz3Qb99bmtcIwWxtHwR75vAuivZK8k9"
       );
