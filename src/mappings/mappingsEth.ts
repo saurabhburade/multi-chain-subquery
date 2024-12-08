c72d19ff78eae2dbed8acc56c445664e4e30772b
Saurabh Burade <sburade3@gmail.com>
2024-12-08 17:32:31 +0530

    fix: build

--- src/mappings/mappingsEth.ts
+++ src/mappings/mappingsEth.ts
@@ -51,44 +51,44 @@ export async function handleEthBlock(block: EthereumBlock): Promise<void> {
 export async function handleOraclePricePerMinute(
   block: EthereumBlock
 ): Promise<void> {
-  const oracleContract = OneinchABIAbi__factory.connect(
-    ORACLE_ADDRESS,
-    // @ts-ignore
-    api as any
-  );
-  const eth = await oracleContract.getRate(
-    "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", // WETH
-    "0xdac17f958d2ee523a2206206994597c13d831ec7", // USDT
-    false
-  );
-  const avail = await oracleContract.getRate(
-    "0xEeB4d8400AEefafC1B2953e0094134A887C76Bd8", // WETH
-    "0xdac17f958d2ee523a2206206994597c13d831ec7", // USDT
-    false
-  );
-  const blockDate = new Date(Number(block.timestamp) * 1000);
-  const minuteId = Math.floor(blockDate.getTime() / 60000);
-  let priceFeedMinute = await PriceFeedMinute.get(minuteId.toString());
-  if (priceFeedMinute === undefined || priceFeedMinute === null) {
-    priceFeedMinute = PriceFeedMinute.create({
-      id: minuteId.toString(),
-      availPrice: avail.toNumber(),
-      ethPrice: eth.toNumber(),
-      blockNumber: block.number,
-      date: new Date(Number(block.timestamp) * 1000),
-    });
-  }
-  priceFeedMinute.availPrice = avail.toNumber();
-  priceFeedMinute.ethPrice = eth.toNumber();
-  priceFeedMinute.blockNumber = block.number;
-
-  priceFeedMinute.date = new Date(Number(block.timestamp) * 1000);
-  // logger.info(
-  //   `New ETH Price Feed Minute::::::  ${priceFeedMinute.ethPrice.toString()} :: ID:: ${minuteId} :: AT:: ${blockDate}`
+  // const oracleContract = OneinchABIAbi__factory.connect(
+  //   ORACLE_ADDRESS,
+  //   // @ts-ignore
+  //   api as any
   // );
-  // logger.info(
-  //   `New AVAIL Price Feed Minute::::::  ${priceFeedMinute.availPrice.toString()} :: ID:: ${minuteId} :: AT:: ${blockDate}`
+  // const eth = await oracleContract.getRate(
+  //   "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", // WETH
+  //   "0xdac17f958d2ee523a2206206994597c13d831ec7", // USDT
+  //   false
   // );
+  // const avail = await oracleContract.getRate(
+  //   "0xEeB4d8400AEefafC1B2953e0094134A887C76Bd8", // WETH
+  //   "0xdac17f958d2ee523a2206206994597c13d831ec7", // USDT
+  //   false
+  // );
+  // const blockDate = new Date(Number(block.timestamp) * 1000);
+  // const minuteId = Math.floor(blockDate.getTime() / 60000);
+  // let priceFeedMinute = await PriceFeedMinute.get(minuteId.toString());
+  // if (priceFeedMinute === undefined || priceFeedMinute === null) {
+  //   priceFeedMinute = PriceFeedMinute.create({
+  //     id: minuteId.toString(),
+  //     availPrice: avail.toNumber(),
+  //     ethPrice: eth.toNumber(),
+  //     blockNumber: block.number,
+  //     date: new Date(Number(block.timestamp) * 1000),
+  //   });
+  // }
+  // priceFeedMinute.availPrice = avail.toNumber();
+  // priceFeedMinute.ethPrice = eth.toNumber();
+  // priceFeedMinute.blockNumber = block.number;
 
-  return await priceFeedMinute.save();
+  // priceFeedMinute.date = new Date(Number(block.timestamp) * 1000);
+  // // logger.info(
+  // //   `New ETH Price Feed Minute::::::  ${priceFeedMinute.ethPrice.toString()} :: ID:: ${minuteId} :: AT:: ${blockDate}`
+  // // );
+  // // logger.info(
+  // //   `New AVAIL Price Feed Minute::::::  ${priceFeedMinute.availPrice.toString()} :: ID:: ${minuteId} :: AT:: ${blockDate}`
+  // // );
+
+  // return await priceFeedMinute.save();
 }
